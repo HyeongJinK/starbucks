d5034cf2ff0ca6bb1dbf889ec638373959062f4d
HyeongJinK <wkzkfmxk23@gmail.com>
2021-01-27 17:15:00 +0900

    :heavy_plus_sign: 주문 타입 추가

--- src/main/java/khj/pilot/Order.java
+++ src/main/java/khj/pilot/Order.java
@@ -8,19 +8,27 @@ import java.util.List;
  * 주문
  * */
 public class Order {
-    List<Beverage> orders = new ArrayList<>();
+    List<Beverage> beverages = new ArrayList<>();
     LocalDateTime orderDate;
+    OrderType orderType;
 
+    /**
+     * 주문 시작하기
+     * */
+    public Order() {
+        orderType = OrderType.ON_ORDER;
+    }
     /**
      *  음료 추가하기
      * */
     public void addOrder(Beverage beverage) {
-        orders.add(beverage);
+        beverages.add(beverage);
     }
     /**
      * 주문 완료
      * */
     public void completeOrder() {
         orderDate = LocalDateTime.now();
+        orderType = OrderType.COMPLETE;
     }
 }
---
+++ src/main/java/khj/pilot/OrderType.java
@@ -0,0 +1,11 @@
+package khj.pilot;
+
+public enum OrderType {
+    ON_ORDER("주문 중"), MAKING("만드는 중"), COMPLETE("완료");
+
+    private String name;
+
+    OrderType(String name) {
+        this.name = name;
+    }
+}
