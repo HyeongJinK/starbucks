41543d71b6a978411f840dfc70af064cfe77ab63
HyeongJinK <wkzkfmxk23@gmail.com>
2021-01-28 13:50:56 +0900

    :heavy_plus_sign: test #5 주문금액 계산하기

---
+++ src/test/java/khj/pilot/order/OrderTest.java
@@ -0,0 +1,25 @@
+package khj.pilot.order;
+
+import khj.pilot.product.Product;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OrderTest {
+
+    @Test
+    public void totalPrice() {
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product(0, "test", BigDecimal.valueOf(1000l)));
+        productList.add(new Product(1, "test2", BigDecimal.valueOf(2000l)));
+        Order order = new Order(productList);
+
+        assertEquals("주문금액이 맞지 않습니다.", order.getTotalPrice().longValue()
+                , BigDecimal.valueOf(3000l).longValue());
+    }
+}
