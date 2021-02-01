f0816eae094db1bb55707ca5cda624b68fe4920d
HyeongJinK <wkzkfmxk23@gmail.com>
2021-02-01 15:35:32 +0900

    :white_check_mark: test #3 상품 주문 테스트 코드

--- src/test/java/khj/pilot/order/OrderTest.java
+++ src/test/java/khj/pilot/order/OrderTest.java
@@ -1,6 +1,7 @@
 package khj.pilot.order;
 
 import khj.pilot.product.Product;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.Array;
@@ -11,15 +12,32 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class OrderTest {
-
-    @Test
-    public void totalPriceTest() {
+    Order order;
+    @Before
+    public void setUp() {
         List<Product> productList = new ArrayList<>();
         productList.add(new Product(0, "test", BigDecimal.valueOf(1000l)));
         productList.add(new Product(1, "test2", BigDecimal.valueOf(2000l)));
-        Order order = new Order(productList);
+        order = new Order(productList);
+    }
 
+    @Test
+    public void getTotalPrice() {
         assertEquals("주문금액이 맞지 않습니다.", order.getTotalPrice().longValue()
                 , BigDecimal.valueOf(3000l).longValue());
+    }
+
+    @Test
+    public void making() {
+        assertEquals(order.getOrderType(), OrderType.ON_ORDER);
+        order.making();
+        assertEquals(order.getOrderType(), OrderType.MAKING);
+    }
+
+    @Test
+    public void complete() {
+        assertEquals(order.getOrderType(), OrderType.ON_ORDER);
+        order.complete();
+        assertEquals(order.getOrderType(), OrderType.COMPLETE);
     }
 }
